309049acea87ff2eb714b610cbdd5cae6b4121a4
allysonfel <allysumotion@gmail.com>
2026-05-26 09:45:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,9 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # O Railway define a porta automaticamente através da variável de ambiente PORT
+    # Se ele não encontrar a variável (como no seu computador), ele usa a 8080 por padrão
+    port = int(os.environ.get("PORT", 8080))
+    
+    # Importante: host='0.0.0.0' permite que o Railway torne seu app público
+    app.run(host='0.0.0.0', port=port)
